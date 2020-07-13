39927764f1ceb7885435eac8f10b0640fe8a1470
tatsuya828828 <kkjy011894@gmail.com>
2020-07-13 11:48:12 +0900

    [finish]16-05

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,33 @@
-import java.util.HashSet;
-import java.util.LinkedHashSet;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
 	public static void main(String[] args) {
-		// CollectionインターフェースにはListインターフェース (ArrayListクラスやLinkedListクラス)の他に
-		// Setインターフェース(HashSetクラスやLinkedHashSetクラスやTreeSetクラス)もある
-		// Set関連クラスは複数の情報を重複なく格納する集合(Set)というデータ構造を実現するためのもので
-		// それぞれの要素に順序がないことが一般的である
-		// Setはそれぞれの要素には重複が許されない、また順序関係がない
-		Set<String> colors = new HashSet<>();
-		colors.add("赤");
-		colors.add("青");
-		colors.add("緑");
-		colors.add("黒");
-		// 赤は既に格納されているため格納されない
-		colors.add("赤");
-		System.out.print("HashSet:");
-		// 順序関係がないためget()やset()は使えない
-		// HashSetに格納された値は拡張for文やIteratorが使って取り出せるが取り出される順序は保証されない
-		for(String s: colors) {
-			System.out.print(s +"→");
-		}
+		// Mapインターフェースはキー(key)と値(value)の2つの情報をペアとして格納するデータ構造
+		// 格納した値は、キーを指定して読み書きできる
+		// 値の重複は許されるが、キーの重複は許されない
+		Map<String, Integer> prefs = new HashMap<String, Integer>();
+		// putで格納できる
+		prefs.put("京都府", 255);
+		prefs.put("東京都", 1261);
+		prefs.put("熊本県", 181);
+		// getで取り出すことができる
+		int tokyo = prefs.get("東京都");
+		System.out.println("東京都の人口は、"+ tokyo +"万人です");
+		prefs.remove("京都府");
+		// 値を削除したため表示しようとするとエラーが出る
+		// int kyoto = prefs.get("京都府");
+		// System.out.println("京都府の人口は、"+ kyoto +"万人です");
+		// 再度putで格納することで上書きされる
+		prefs.put("熊本県", 182);
+		int kumamoto = prefs.get("熊本県");
+		System.out.println("熊本県の人口は、"+ kumamoto +"万人です");
 
-		System.out.println();
-		// LinkedHashSetであれば値を格納した順序に整列する
-		Set<String> colors2 = new LinkedHashSet<>();
-		colors2.add("赤");
-		colors2.add("青");
-		colors2.add("緑");
-		colors2.add("黒");
-		System.out.print("LinkedHashSet:");
-		for(String s: colors2) {
-			System.out.print(s +"→");
-		}
-
-		System.out.println();
-		// TreeSetであれば自然順序づけで整列する
-		Set<String> animals = new TreeSet<String>();
-		animals.add("dog");
-		animals.add("cat");
-		animals.add("wolf");
-		animals.add("panda");
-		System.out.print("TreeSet:");
-		for(String s: animals) {
-			System.out.print(s +"→");
+		// まずキー一覧を取得し繰り返す
+		for(String key: prefs.keySet()) {
+			// キーを指定して値を取得
+			int value = prefs.get(key);
+			System.out.println(key+ "の人口は、"+ value +"万人です");
 		}
 	}
 }
