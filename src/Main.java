a3f75d6cbb3c09653139c75222a074845ba58877
tatsuya828828 <kkjy011894@gmail.com>
2020-07-13 12:39:32 +0900

    [finish]16-08

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,29 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-class Hero{
-	public String name;
-}
+import job.Hero;
+
 public class Main {
 	public static void main(String[] args) {
-		Hero h = new Hero();
-		h.name = "ミナト";
-		List<Hero> list = new ArrayList<Hero>();
-		list.add(h);
-		// コレクションへ格納しているのはあくまでもアドレスなので後から変更を加えると反映されてしまう
-		h.name = "スガワラ";
-		System.out.println(list.get(0).name);
+		List<Hero> heros = new ArrayList<>();
+		Hero h = new Hero("斎藤");
+		Hero h2 = new Hero("鈴木");
+		heros.add(h);
+		heros.add(h2);
+
+		for(Hero hero: heros) {
+			System.out.println(hero.getName());
+		}
+
+		Map<String, Integer> heros2 = new HashMap<>();
+		heros2.put(h.getName(), 3);
+		heros2.put(h2.getName(), 7);
+
+		for(String key: heros2.keySet()) {
+			int value = heros2.get(key);
+			System.out.println(key +"が倒した数"+ value +"体");
+		}
 	}
 }
